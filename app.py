97353d7ead8bb897d29fdc15b3e2c80a266b103d
Gaurav Gaurav <zen@Zen-master.local>
2024-08-18 09:19:22 +0100

    Did following changes : 1) Updated FLASK app to include -: a) socket function whihc enables figuring out server ip address dynamcialy b) changed port for flask server to 5001 as 5000 is bieng used by airplay c)added my-bootstrap-toolbox.html file in static folder which contians bootstrap classes feartures used in app till now.

--- app.py
+++ app.py
@@ -4,7 +4,26 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 import shutil
+import webbrowser
+import threading
+import socket
 
+def get_local_ip():
+    """Get the local IP address of the machine."""
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # This doesn't even have to be reachable
+        s.connect(('10.254.254.254', 1))
+        ip_address = s.getsockname()[0]
+    except Exception:
+        ip_address = '127.0.0.1'
+    finally:
+        s.close()
+    return ip_address
+
+def open_browser():
+    local_ip = get_local_ip()
+    webbrowser.open_new(f"http://{local_ip}:5001")
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
@@ -182,5 +201,7 @@ def reset_db():
     flash('Database reset successfully.', 'success')
     return redirect(url_for('admin_dashboard'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # Start a thread to open the browser after the Flask app starts
+    threading.Timer(1, open_browser).start()
+    app.run(host='0.0.0.0', port=5001, debug=True, use_reloader=False)
