c2aca27b67e30c0d8c03ebd6cdc82c02ac73279f
Gaurav Gaurav <zen@Zen-master.local>
2024-09-06 22:23:59 +0100

    Move to integrarting React front end. Inde page working. Some hosuekeeping done and elted several pages from tempalate

--- app/main_routes.py
+++ app/main_routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template,jsonify, request
+from flask import Blueprint, render_template,jsonify, request, flash
 from flask_wtf.csrf import generate_csrf
 from flask import render_template
+from .models import CategoryNames
+from . import db
 #----------------------------------------------------------------
 # Blueprint configuration
 main_bp = Blueprint(
@@ -20,9 +22,31 @@ main_bp = Blueprint(
 
 #----------------------------------------------------------------
 # Route for Home/Index Page
+# Define a route for the index page of the main blueprint
 @main_bp.route('/')
 def index():
-    return render_template('index.html')
+    # Initialize an empty list to hold the formatted category names
+    cat_list = []
+
+    # Loop through each enum member in the CategoryNames enum
+    for enum_object in CategoryNames:
+        # Split the enum member name by underscores into a list of words
+        cat = enum_object.name.split("_")
+        
+        # Capitalize the first letter of each word and join them back into a string
+        capit_cat = [word.capitalize() for word in cat]
+        
+        # Join the capitalized words with spaces to form a readable category name
+        category = " ".join(capit_cat)
+        
+        # Add the formatted category name to the list
+        cat_list.append(category)
+
+    # Create a dictionary mapping each category name to its index
+    cat_dict = {cat: idx for idx, cat in enumerate(cat_list)}
+
+    # Render the 'index.html' template, passing the category dictionary to the template
+    return render_template('index.html', cat_dict=cat_dict)
 #!----------------------------------------------------------------
 #!Debugging routes:
 @main_bp.route('/debug')
