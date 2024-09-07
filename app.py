f0d0fed4b04f487fe0701e86a678318b298d17c7
Gaurav Gaurav <zen@Zen-master.local>
2024-09-08 00:48:26 +0100

    fixed errors in view_document and view_category fucntion, to ensure the display_name is properly passed on.

--- app.py
+++ app.py
@@ -2,15 +2,20 @@
 from app import create_app
 app = create_app()
 
-# Start server;
+# Imports for threading, webbrowser, and socket to open the browser automatically
 import threading
 import webbrowser
 import socket
 
+# Function to open the default web browser
+def open_browser():
+    # Find the free port on localhost to open in the web browser
+    port = 5001  # Make sure this matches the port in app.run()
+    url = f"http://127.0.0.1:{port}"
+    webbrowser.open_new(url)
+
 # Start the application
 if __name__ == "__main__":
     # Start a thread to open the browser after the Flask app starts
-    threading.Timer(1, open_browser).start()
+    threading.Timer(1, open_browser).start()  # Wait 1 second before opening the browser
     app.run(host='0.0.0.0', port=5001, debug=True, use_reloader=True)
-
-
