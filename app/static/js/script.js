f528927a9204e6bf9f0e49a1eebcd87e069e6e0f
Gaurav Gaurav <zen@Zen-master.local>
2024-09-08 02:44:57 +0100

    ver-1.2 functional - removed carousel function and reverted to grid in smaller screens

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -116,86 +116,73 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
 // Functions to inject carousel when screen size is small.
 // Functions to inject carousel when screen size is small.
-function initCarousel() {
-    $('.my-card-canvas').addClass('carousel slide').attr('data-bs-ride', 'carousel');
-    $('.my-card-box').addClass('carousel-inner').removeClass('row');
-    $('.card-img').removeClass('img-fluid rounded-start').addClass('carousel-img');
-    $('.my-card-item').removeClass('col-md-6 col-lg-4 mb-4').addClass('carousel-item').first().addClass('active');
+//function initCarousel() {
+//    $('.my-card-canvas').addClass('carousel slide').attr('data-bs-ride', 'carousel');
+//    $('.my-card-box').addClass('carousel-inner').removeClass('row');
+//    $('.card-img').removeClass('img-fluid rounded-start').addClass('carousel-img');
+//    $('.my-card-item').removeClass('col-md-6 col-lg-4 mb-4').addClass('carousel-item').first().addClass('active');
+//
+//    $('#cardContainer').append(`
+//        <button class="carousel-control-prev" type="button" data-bs-target="#cardContainer" data-bs-slide="prev">
+//            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+//            <span class="visually-hidden">Previous</span>
+//        </button>
+//        <button class="carousel-control-next" type="button" data-bs-target="#cardContainer" data-bs-slide="next">
+//            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+//            <span class="visually-hidden">Next</span>
+//        </button>
+//    `);
+//
+//    // Initialize the carousel
+//    $('#cardContainer').carousel();
+//}
 
-    $('#cardContainer').append(`
-        <button class="carousel-control-prev" type="button" data-bs-target="#cardContainer" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#cardContainer" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    `);
+//function destroyCarousel() {
+//    $('.my-card-canvas').removeClass('carousel slide').removeAttr('data-bs-ride');
+//    $('.my-card-box').removeClass('carousel-inner').addClass('row');
+//    $('.my-card-item').removeClass('carousel-item active').addClass('col-md-6 col-lg-4 mb-4');
+//    $('.card-img').removeClass('carousel-img').addClass('img-fluid rounded-start');
+//    
+//    // Removing carousel controls and indicators
+ //   $('#cardContainer .carousel-control-prev, #cardContainer .carousel-control-next').remove();
+//    $('#cardContainer .carousel-indicators').remove();
+//}
 
-    // Initialize the carousel
-    $('#cardContainer').carousel();
-}
-
-function destroyCarousel() {
-    $('.my-card-canvas').removeClass('carousel slide').removeAttr('data-bs-ride');
-    $('.my-card-box').removeClass('carousel-inner').addClass('row');
-    $('.my-card-item').removeClass('carousel-item active').addClass('col-md-6 col-lg-4 mb-4');
-    $('.card-img').removeClass('carousel-img').addClass('img-fluid rounded-start');
-    
-    // Removing carousel controls and indicators
-    $('#cardContainer .carousel-control-prev, #cardContainer .carousel-control-next').remove();
-    $('#cardContainer .carousel-indicators').remove();
-}
-
-function checkScreenWidth() {
-    if ($(window).width() <= 468) {
-        if (!$('.my-card-box').hasClass('carousel-inner')) {
-            initCarousel();
-            const carousel = new bootstrap.Carousel('#cardContainer');
-            
-        }
-    } else {
-        if ($('.my-card-box').hasClass('carousel-inner')) {
-            destroyCarousel();
-        }
-    }
-}
-
-$(document).ready(function () {
-    checkScreenWidth();
-    $(window).on('resize', checkScreenWidth);
-    $('#cardContainer').carousel();  // Initialize the carousel
-});
 
 // Function to handle the class switching based on screen size
 function updateGridClass() {
     var element = document.querySelector('.row.my-frame');
     
     if (element) {  // Ensure the element exists
-        if (window.innerWidth < 468) {
-            // If the screen width is less than 468px, remove g-5 and add g-3
+    if (window.innerWidth < 468) {
+            //If the screen width is less than 468px, remove g-5 and add g-3
             if (element.classList.contains('g-5')) {
                 element.classList.remove('g-5');
                 element.classList.add('g-3');
             }
         } else {
             // If the screen width is greater than or equal to 468px, revert to g-5
-            if (element.classList.contains('g-3')) {
+                if (element.classList.contains('g-3')) {
                 element.classList.remove('g-3');
                 element.classList.add('g-5');
             }
         }
     }
 }
-// Listen for resize events and apply the function
+//Listen for resize events and apply the function
 window.addEventListener('resize', updateGridClass);
 
 // Call the function once when the script loads to set the initial state
 updateGridClass();
- // JavaScript function to confirm the reset action
+
+// JavaScript function to confirm the reset action
+
 function ConfirmReset() {
     return confirm('Are you sure you want to reset the users? This action cannot be undone.');
 }
+
+
+// TOggle button to shift between list and coarusel view 
