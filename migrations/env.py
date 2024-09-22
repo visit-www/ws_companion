7ea57349d1d65bf8181271a18340ad61f4c4d1c2
Gaurav Gaurav <zen@Zen-master.local>
2024-09-22 15:55:18 +0100

    Clean state before generating initial migration

--- migrations/env.py
+++ migrations/env.py
@@ -1,13 +1,9 @@
-from app.models import Base  # Import the Base object from your models.py
-from app.models import User, Content, Reference, AdminReportTemplate, UserData, UserContentState, UserReportTemplate, UserProfile, UserFeedback
-
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-import os
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -17,25 +13,12 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
-# Here’s the key change:
-# Check if we are running on Heroku (using DATABASE_URL)
-#If DATABASE_URL is found, override the sqlalchemy.url in alembic.ini
-# Fetch DATABASE_URL and replace postgres:// with postgresql://
-database_url = os.getenv("DATABASE_URL", "postgresql://admin:811976@localhost:5432/wscdb")
-
-if database_url and database_url.startswith("postgres://"):
-    database_url = database_url.replace("postgres://", "postgresql://", 1)
-
-config.set_main_option("sqlalchemy.url", database_url)
-print("Database URL:", database_url)
-
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
-
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -82,18 +65,14 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-            version_table_schema="public"  # Set the default schema to public
+            connection=connection, target_metadata=target_metadata
         )
 
         with context.begin_transaction():
             context.run_migrations()
 
 
-
 if context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
-
